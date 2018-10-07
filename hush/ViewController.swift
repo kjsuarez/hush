58b8f1c93093441146164c65ecf4ba138727f8e5
kjsuarez <kjsuarez94@gmail.com>
2018-10-07 11:13:00 -0700

    Initial Commit

---
+++ hush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hush
+//
+//  Created by Kevin Suarez on 10/7/18.
+//  Copyright © 2018 Kevin Suarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
